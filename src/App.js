e187e057d74bae0745f70b33ba9941dea11cf111
brohan2001 <rohan.balasubramanian@gmail.com>
2022-02-16 22:14:41 +0530

    basic template made

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Layout } from "antd";
+import "antd/dist/antd.css";
+import Header from "./components/header/Header";
+import Body from "./components/body/Body";
+import AppCSS from "./App.module.css";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from "./store/reducer";
+import devToolsEnhancer from "remote-redux-devtools";
+import Footer from "./components/footer/Footer";
+import MetaTags from "react-meta-tags";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor() {
+		super();
+		this.developmentMode = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+		if (this.developmentMode) {
+			this.store = createStore(reducer, devToolsEnhancer());
+		} else {
+			this.store = createStore(reducer);
+		}
+	}
+	render() {
+		return (
+			<Provider store={this.store}>
+				{this.developmentMode ? null : (
+					<MetaTags>
+						<meta
+							httpEquiv="Content-Security-Policy"
+							content="default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; object-src 'none'; style-src https://fonts.googleapis.com https://cryption.pw 'unsafe-inline'; img-src 'self'; media-src 'none'; frame-src 'none'; font-src https://fonts.gstatic.com; connect-src 'none'"
+						/>
+						<meta
+							httpEquiv="X-Content-Security-Policy"
+							content="default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; object-src 'none'; style-src https://fonts.googleapis.com https://cryption.pw 'unsafe-inline'; img-src 'self'; media-src 'none'; frame-src 'none'; font-src https://fonts.gstatic.com; connect-src 'none'"
+						/>
+						<meta
+							httpEquiv="X-WebKit-CSP"
+							content="default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; object-src 'none'; style-src https://fonts.googleapis.com https://cryption.pw 'unsafe-inline'; img-src 'self'; media-src 'none'; frame-src 'none'; font-src https://fonts.gstatic.com; connect-src 'none'"
+						/>
+					</MetaTags>
+				)}
+				<Layout className={AppCSS.layout}>
+					<Header />
+					<Body />
+					<Footer />
+				</Layout>
+			</Provider>
+		);
+	}
 }
 
 export default App;
